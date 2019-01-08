901cad6c4eb300d7c599abe441591e4572b85996
deijai.almeida <deijai.almeida@SA01-TI01.superadega01.local>
2019-01-08 09:32:48 -0200

    adicionar projeto

---
+++ bootstrap/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>BOOTSTRAP 4</title>
+	<meta charset="utf-8">
+	<meta name = "viewport" content="width=device-width, initial-scale=1,shrink-to-fit=no" />
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css" />
+	<link rel="stylesheet" type="text/css" href="css/css.css" />
+</head>
+<body>
+
+	<div class="container">
+		<h1>Sitema com Bootstrap <small>Sistema de GRID</small></h1>
+		<p>Sistema desenvolvido com <mark>Bootstrap</mark></p>
+		<hr>
+
+
+		<div class="alert alert-danger" role="alert">
+		  A simple primary alert with <a href="#" class="alert-link">an example link</a>. Give it a click if you like.
+			 <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+			  <span aria-hidden="true">&times;</span>
+			</button>
+		</div>
+		
+
+
+
+
+
+
+	</div>
+
+
+	<script type="text/javascript" src="js/jquery.js"></script>
+	<script type="text/javascript" src="js/bootstrap.bundle.min.js"></script>
+
+</body>
+</html>
